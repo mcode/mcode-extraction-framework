20455d74346e6801853fd61eb6e3ed0d339fc7f1
Dylan Phelan <dtphelan1@gmail.com>
2020-05-08 11:11:24 -0400

    Updates based on MG comments

--- src/extractors/CSVCancerDiseaseStatusExtractor.js
+++ src/extractors/CSVCancerDiseaseStatusExtractor.js
@@ -8,8 +8,8 @@ function joinAndReformatData(arrOfDiseaseStatusData) {
   // No join needed - just reformatting for template
   // Check the shape of the data
   arrOfDiseaseStatusData.forEach((record) => {
-    if (!(record.mrn && record.subjectId && record.conditionFHIRId && record.diseaseStatus && record.dateOfObservation)) {
-      throw new Error('DiseaseStatusData missing an expected property: mrn, subjectId, conditionFHIRId, diseaseStatus and dateOfObservation are required.');
+    if (!(record.mrn && record.subjectId && record.conditionId && record.diseaseStatus && record.dateOfObservation)) {
+      throw new Error('DiseaseStatusData missing an expected property: mrn, subjectId, conditionId, diseaseStatus and dateOfObservation are required.');
     }
   });
   return arrOfDiseaseStatusData.map((record) => ({
@@ -24,7 +24,7 @@ function joinAndReformatData(arrOfDiseaseStatusData) {
       id: record.subjectId,
     },
     condition: {
-      id: record.conditionFHIRId,
+      id: record.conditionId,
     },
     effectiveDateTime: record.dateOfObservation,
   }));
