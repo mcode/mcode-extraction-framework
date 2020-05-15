043eba257ba3e5301ad78d6c6bba5d0f64919d51
Julia Afeltra <jafeltra@mitre.org>
2020-05-15 15:22:11 -0400

    Additional exports to support clinical trial extraction in separate repo

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ const {
   CSVTreatmentPlanChangeExtractor,
   Extractor,
 } = require('./extractors');
-const { BaseFHIRModule } = require('./modules');
+const { BaseFHIRModule, CSVModule } = require('./modules');
 const { getPatientName } = require('./helpers/patientUtils');
-const { isBundleEmpty, firstResourceInBundle, allResourcesInBundle } = require('./helpers/fhirUtils');
+const { allResourcesInBundle, firstEntryInBundle, firstResourceInBundle, isBundleEmpty } = require('./helpers/fhirUtils');
 const { generateMcodeResources } = require('./helpers/ejsUtils');
 const { isConditionPrimary, isConditionSecondary, getICD10Code } = require('./helpers/conditionUtils');
 const { getDiseaseStatusCode } = require('./helpers/diseaseStatusUtils');
 
 module.exports = {
   BaseFHIRExtractor,
+  CSVModule,
   BaseFHIRModule,
   CSVCancerDiseaseStatusExtractor,
   CSVClinicalTrialInformationExtractor,
@@ -24,6 +25,7 @@ module.exports = {
   Extractor,
   logger,
   allResourcesInBundle,
+  firstEntryInBundle,
   firstResourceInBundle,
   getDiseaseStatusCode,
   generateMcodeResources,
