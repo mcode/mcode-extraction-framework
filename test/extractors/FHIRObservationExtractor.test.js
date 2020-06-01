5d9f5ea835977fef50f5cd05df52c6d81e711f64
Dylan Phelan <dtphelan1@gmail.com>
2020-06-01 15:20:12 -0400

    Updated test to check for base category if none provided as arg

--- test/extractors/FHIRObservationExtractor.test.js
+++ test/extractors/FHIRObservationExtractor.test.js
@@ -1,5 +1,7 @@
+const rewire = require('rewire');
 const { FHIRObservationExtractor } = require('../../src/extractors/FHIRObservationExtractor.js');
 
+const FHIRObservationExtractorRewired = rewire('../../src/extractors/FHIRObservationExtractor.js');
 const MOCK_URL = 'http://example.com';
 const MOCK_HEADERS = {};
 const MOCK_CATEGORIES = 'category1,category2';
@@ -12,5 +14,9 @@ describe('FHIRObservationExtractor', () => {
   test('Constructor sets category if provided', () => {
     const extractorWithCategories = new FHIRObservationExtractor({ baseFhirUrl: MOCK_URL, requestHeaders: MOCK_HEADERS, category: MOCK_CATEGORIES });
     expect(extractorWithCategories.category).toEqual(MOCK_CATEGORIES);
+  });
+  test('Constructor sets category is based on BASECATEGORY if not provided', () => {
+    const baseCategories = FHIRObservationExtractorRewired.__get__('BASE_CATEGORIES');
+    expect(extractor.category).toEqual(baseCategories);
   });
 });
