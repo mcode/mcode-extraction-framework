e627af7d4fa8d3f506c67bc7a522f17fb0725755
Dylan Phelan <dtphelan1@gmail.com>
2020-06-02 11:37:23 -0400

    updated tests for fhirObservationExt to check for new param value

--- test/extractors/FHIRObservationExtractor.test.js
+++ test/extractors/FHIRObservationExtractor.test.js
@@ -1,22 +1,38 @@
 const rewire = require('rewire');
 const { FHIRObservationExtractor } = require('../../src/extractors/FHIRObservationExtractor.js');
+const examplePatientBundle = require('./fixtures/patient-bundle.json');
 
 const FHIRObservationExtractorRewired = rewire('../../src/extractors/FHIRObservationExtractor.js');
 const MOCK_URL = 'http://example.com';
 const MOCK_HEADERS = {};
+const MOCK_MRN = '123456789';
 const MOCK_CATEGORIES = 'category1,category2';
 
+// Construct extractor and create spies for mocking responses
 const extractor = new FHIRObservationExtractor({ baseFhirUrl: MOCK_URL, requestHeaders: MOCK_HEADERS });
+const extractorWithCategories = new FHIRObservationExtractor({ baseFhirUrl: MOCK_URL, requestHeaders: MOCK_HEADERS, category: MOCK_CATEGORIES });
+const baseCategories = FHIRObservationExtractorRewired.__get__('BASE_CATEGORIES');
+
 describe('FHIRObservationExtractor', () => {
   test('Constructor sets resourceType as Observation', () => {
     expect(extractor.resourceType).toEqual('Observation');
   });
+  test('Constructor sets category is based on BASECATEGORY if not provided', () => {
+    expect(extractor.category).toEqual(baseCategories);
+  });
   test('Constructor sets category if provided', () => {
-    const extractorWithCategories = new FHIRObservationExtractor({ baseFhirUrl: MOCK_URL, requestHeaders: MOCK_HEADERS, category: MOCK_CATEGORIES });
     expect(extractorWithCategories.category).toEqual(MOCK_CATEGORIES);
   });
-  test('Constructor sets category is based on BASECATEGORY if not provided', () => {
-    const baseCategories = FHIRObservationExtractorRewired.__get__('BASE_CATEGORIES');
-    expect(extractor.category).toEqual(baseCategories);
+
+  test('parametrizeArgsForFHIRModule should add category to param values', async () => {
+    // Create spy
+    const { baseFHIRModule } = extractor;
+    const baseFHIRModuleSearchSpy = jest.spyOn(baseFHIRModule, 'search');
+    baseFHIRModuleSearchSpy
+      .mockReturnValue(examplePatientBundle);
+
+    const params = await extractor.parametrizeArgsForFHIRModule({ mrn: MOCK_MRN });
+    expect(params).toHaveProperty('category');
+    expect(params.category).toEqual(baseCategories);
   });
 });
