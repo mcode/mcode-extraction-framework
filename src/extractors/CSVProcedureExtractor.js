22d1e2127bf971587d16cf4b2a0be944cf72edf6
Dan Lee <danlee1025@gmail.com>
2020-10-19 19:42:38 -0400

    Added CSV Procedure Extractor and template.

---
+++ src/extractors/CSVProcedureExtractor.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const { CSVModule } = require('../modules');
+const { generateMcodeResources } = require('../templates');
+const { Extractor } = require('./Extractor');
+const logger = require('../helpers/logger');
+const { formatDateTime } = require('../helpers/dateUtils');
+
+// Formats data to be passed into template-friendly format
+function formatData(procedureData) {
+  logger.debug('Reformatting procedure data from CSV into template format');
+  return procedureData.map((data) => {
+    const {
+      mrn, procedureId, conditionId, status, code, codeSystem, displayName, reasonCode, reasonCodeSystem, reasonDisplayName, bodySite, laterality, effectiveDate, treatmentIntent,
+    } = data;
+
+    if (!(mrn && procedureId && status && code && codeSystem && effectiveDate)) {
+      throw new Error('The procedure is missing an expected attribute. Procedure id, mrn, code system, code, status and effective date are all required.');
+    }
+    return {
+      id: procedureId,
+      subjectId: mrn,
+      status,
+      code,
+      system: codeSystem,
+      display: !displayName ? null : displayName,
+      reasonCode,
+      reasonCodeSystem,
+      reasonDisplayName,
+      conditionId,
+      bodySite,
+      laterality,
+      effectiveDateTime: formatDateTime(effectiveDate),
+      treatmentIntent,
+    };
+  });
+}
+
+class CSVProcedureExtractor extends Extractor {
+  constructor({ filePath }) {
+    super();
+    this.csvModule = new CSVModule(path.resolve(filePath));
+  }
+
+  async getProcedureData(mrn) {
+    logger.debug('Getting Procedure Data');
+    return this.csvModule.get('mrn', mrn);
+  }
+
+  async get({ mrn }) {
+    const procedureData = await this.getProcedureData(mrn);
+    const formattedData = formatData(procedureData);
+
+    return generateMcodeResources('Procedure', formattedData);
+  }
+}
+
+module.exports = {
+  CSVProcedureExtractor,
+};
---
+++ src/templates/ProcedureTemplate.js
@@ -0,0 +1,69 @@
+const {
+  bodySiteTemplate,
+  coding,
+  extensionArr,
+  reference,
+  valueCodeableConcept,
+} = require('./snippets');
+const { ifSomeArgsObj } = require('../helpers/templateUtils');
+
+function reasonTemplate({ reasonCode, reasonCodeSystem, reasonDisplayName }) {
+  return {
+    reasonCode: {
+      coding: [
+        coding({
+          system: reasonCodeSystem,
+          code: reasonCode,
+          display: reasonDisplayName,
+        }),
+      ],
+    },
+  };
+}
+
+function reasonReference(conditionId) {
+  return {
+    reasonReference: [
+      ...reference({ id: conditionId }),
+    ],
+  };
+}
+
+function treatmentIntentTemplate(treatmentIntent) {
+  return {
+    url: 'http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-treatment-intent',
+    ...valueCodeableConcept({
+      system: 'http://snomed.info/sct',
+      code: treatmentIntent,
+    }),
+  };
+}
+
+function procedureTemplate({
+  id, subjectId, status, code, system, display, reasonCode, reasonCodeSystem, reasonDisplayName, conditionId, bodySite, laterality, effectiveDateTime, treatmentIntent,
+}) {
+  if (!(id && subjectId && status && code && system && effectiveDateTime)) {
+    throw Error('Trying to render a ProcedureTemplate, but a required argument is missing; ensure that id, subjectId, status, code, system, and effectiveDateTime are all present');
+  }
+
+  return {
+    resourceType: 'Procedure',
+    id,
+    status,
+    code: {
+      coding: [
+        coding({ code, system, display }),
+      ],
+    },
+    subject: reference({ id: subjectId }),
+    performedDateTime: effectiveDateTime,
+    ...ifSomeArgsObj(reasonTemplate)({ reasonCode, reasonCodeSystem, reasonDisplayName }),
+    ...(conditionId && reasonReference(conditionId)),
+    ...extensionArr(...treatmentIntentTemplate(treatmentIntent)),
+    ...ifSomeArgsObj(bodySiteTemplate)({ bodySite, laterality }),
+  };
+}
+
+module.exports = {
+  procedureTemplate,
+};
