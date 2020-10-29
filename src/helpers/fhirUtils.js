1bc376084583429e0f8325baaf36081058685f33
Julia Afeltra <jafeltra@mitre.org>
2020-10-29 15:33:19 -0400

    Move initial info log outside of loop

--- src/helpers/fhirUtils.js
+++ src/helpers/fhirUtils.js
@@ -98,6 +98,7 @@ const getBundleEntriesByResourceType = (bundle, type, context = {}, first = fals
 };
 
 const logOperationOutcomeInfo = (operationOutcome) => {
+  logger.info('An OperationOutcome was returned with the following issue(s):');
   operationOutcome.issue.forEach((issue) => {
     let issueMessage = `Severity: ${issue.severity}. Code: ${issue.code}`;
     let detailsMessage = '';
@@ -118,7 +119,6 @@ const logOperationOutcomeInfo = (operationOutcome) => {
     } else if (issue.severity === 'warning') {
       logLevel = 'warn';
     }
-    logger.log(logLevel, 'An OperationOutcome was returned with the following issue(s):');
     logger.log(logLevel, issueMessage);
     if (detailsMessage) {
       // If there were any codes, log them
