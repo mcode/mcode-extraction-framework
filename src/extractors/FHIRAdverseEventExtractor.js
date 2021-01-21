e993578bbd4f1a1db1e86577e4016623208e60e3
mickohanlon23 <mohanlon@mitre.org>
2021-01-21 18:53:57 -0500

    Switch patient value to subject and add testing for this functionality

--- src/extractors/FHIRAdverseEventExtractor.js
+++ src/extractors/FHIRAdverseEventExtractor.js
@@ -1,9 +1,25 @@
 const { BaseFHIRExtractor } = require('./BaseFHIRExtractor');
 
+const BASE_STUDY = ''; // No base study specified, however a study field is required for Epic calls
+
 class FHIRAdverseEventExtractor extends BaseFHIRExtractor {
-  constructor({ baseFhirUrl, requestHeaders, version }) {
+  constructor({ baseFhirUrl, requestHeaders, version, study }) {
     super({ baseFhirUrl, requestHeaders, version });
     this.resourceType = 'AdverseEvent';
+    this.study = study || BASE_STUDY;
+  }
+
+  // In addition to default parametrization, add study if specified
+  async parametrizeArgsForFHIRModule({ mrn, context }) {
+    const paramsWithID = await super.parametrizeArgsForFHIRModule({ mrn, context });
+    // Patient is referenced in the 'subject' field of AdverseEvent, which is required for Epic calls
+    paramsWithID.subject = paramsWithID.patient;
+    delete paramsWithID.patient;
+    // Only add study to parameters if it has been specified
+    return {
+      ...paramsWithID,
+      ...(this.study && { study: this.study }),
+    };
   }
 }
 
--- test/extractors/FHIRAdverseEventExtractor.test.js
+++ test/extractors/FHIRAdverseEventExtractor.test.js
@@ -1,11 +1,64 @@
+const rewire = require('rewire');
 const { FHIRAdverseEventExtractor } = require('../../src/extractors/FHIRAdverseEventExtractor');
+const examplePatientBundle = require('./fixtures/patient-bundle.json');
 
+const FHIRAdverseEventExtractorRewired = rewire('../../src/extractors/FHIRAdverseEventExtractor.js');
 const MOCK_URL = 'http://example.com';
 const MOCK_HEADERS = {};
+const MOCK_MRN = '123456789';
+const MOCK_STUDIES = 'study1,study2';
 
+// Construct extractor and create sppies for mocking responses
 const extractor = new FHIRAdverseEventExtractor({ baseFhirUrl: MOCK_URL, requestHeaders: MOCK_HEADERS });
+const extractorWithStudy = new FHIRAdverseEventExtractor({ baseFhirUrl: MOCK_URL, requestHeaders: MOCK_HEADERS, study: MOCK_STUDIES });
+const baseStudy = FHIRAdverseEventExtractorRewired.__get__('BASE_STUDY');
+
 describe('FHIRAdverseEventExtractor', () => {
-  test('Constructor sets resourceType as AdverseEvent', () => {
-    expect(extractor.resourceType).toEqual('AdverseEvent');
+  describe('Constructor', () => {
+    test('Constructor sets resourceType as AdverseEvent', () => {
+      expect(extractor.resourceType).toEqual('AdverseEvent');
+    });
+    test('sets study based on BASE_STUDY if not provided', () => {
+      expect(extractor.study).toEqual(baseStudy);
+    });
+    test('sets status if provided', () => {
+      expect(extractorWithStudy.study).toEqual(MOCK_STUDIES);
+    });
+  });
+
+  describe('parametrizeArgsForFHIRModule', () => {
+    test('should not add study when not set to param values', async () => {
+      // Create spy
+      const { baseFHIRModule } = extractor;
+      const baseFHIRModuleSearchSpy = jest.spyOn(baseFHIRModule, 'search');
+      baseFHIRModuleSearchSpy
+        .mockReturnValue(examplePatientBundle);
+
+      const params = await extractor.parametrizeArgsForFHIRModule({ mrn: MOCK_MRN });
+      expect(params).not.toHaveProperty('study');
+    });
+
+    test('should add study when set to param values', async () => {
+      // Create spy
+      const { baseFHIRModule } = extractorWithStudy;
+      const baseFHIRModuleSearchSpy = jest.spyOn(baseFHIRModule, 'search');
+      baseFHIRModuleSearchSpy
+        .mockReturnValue(examplePatientBundle);
+
+      const params = await extractorWithStudy.parametrizeArgsForFHIRModule({ mrn: MOCK_MRN });
+      expect(params).toHaveProperty('study');
+      expect(params.study).toEqual(extractorWithStudy.study);
+    });
+
+    test('should delete patient after its value is moved to subject', async () => {
+      // Create spy
+      const { baseFHIRModule } = extractorWithStudy;
+      const baseFHIRModuleSearchSpy = jest.spyOn(baseFHIRModule, 'search');
+      baseFHIRModuleSearchSpy
+        .mockReturnValue(examplePatientBundle);
+
+      const params = await extractorWithStudy.parametrizeArgsForFHIRModule({ mrn: MOCK_MRN });
+      expect(params).not.toHaveProperty('patient');
+    });
   });
 });
