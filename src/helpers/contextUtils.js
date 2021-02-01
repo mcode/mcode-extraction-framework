1b689a53322e6c5ab17c822d53047434859a6228
Dylan Phelan <dtphelan1@gmail.com>
2021-02-01 11:02:37 -0500

    Update src/helpers/contextUtils.js

--- src/helpers/contextUtils.js
+++ src/helpers/contextUtils.js
@@ -2,6 +2,7 @@ const logger = require('./logger');
 const { getBundleResourcesByType } = require('./fhirUtils');
 
 async function getPatientFromContext(mrn, context) {
+  logger.debug('Getting patient from context');
   const patientInContext = getBundleResourcesByType(context, 'Patient', {}, true);
   if (!patientInContext) {
     throw Error('Could not find a patient in context; ensure that a PatientExtractor is used earlier in your extraction configuration');
