cf1089d100511f7f2c8fe1d0076f8896bd6bea9e
Dylan Phelan <dtphelan1@gmail.com>
2021-05-19 15:12:27 -0400

    Create singl string-normalizer function; used on unalterableColumns

--- src/modules/CSVModule.js
+++ src/modules/CSVModule.js
@@ -3,27 +3,34 @@ const moment = require('moment');
 const parse = require('csv-parse/lib/sync');
 const logger = require('../helpers/logger');
 
+// The standard string normalizer function
+function stringNormalizer(str) {
+  return str.toLowerCase();
+}
+
+// For translating null/nil-like values into empty strings
 function normalizeEmptyValues(data, unalterableColumns = []) {
-  const EMPTY_VALUES = ['null', 'nil'].map((v) => v.toLowerCase());
-  // Flag tracking if data was normalized or not.
-  let wasNormalized = false;
+  const EMPTY_VALUES = ['null', 'nil'].map(stringNormalizer);
+  const normalizedUnalterableColumns = unalterableColumns.map(stringNormalizer);
+  // Flag tracking if empty values were normalized or not.
+  let wasEmptyNormalized = false;
   const newData = data.map((row, i) => {
     const newRow = { ...row };
     // Filter out unalterable columns
-    const columnsToNormalize = Object.keys(row).filter((col) => !unalterableColumns.includes(col));
+    const columnsToNormalize = Object.keys(row).filter((col) => !normalizedUnalterableColumns.includes(stringNormalizer(col)));
     columnsToNormalize.forEach((col) => {
       const value = newRow[col];
       // If the value for this row-col combo is a value that should be empty, replace it
-      if (EMPTY_VALUES.includes(value.toLowerCase())) {
+      if (EMPTY_VALUES.includes(stringNormalizer(value))) {
         logger.debug(`NULL/NIL values '${value}' found in row-${i}, col-${col}`);
-        wasNormalized = true;
+        wasEmptyNormalized = true;
         newRow[col] = '';
       }
     });
     return newRow;
   });
 
-  if (wasNormalized) {
+  if (wasEmptyNormalized) {
     logger.warn('NULL/NIL values found and replaced with empty-strings');
   }
   return newData;
@@ -33,7 +40,7 @@ class CSVModule {
   constructor(csvFilePath, unalterableColumns) {
     // Parse then normalize the data
     const parsedData = parse(fs.readFileSync(csvFilePath), {
-      columns: (header) => header.map((column) => column.toLowerCase()),
+      columns: (header) => header.map((column) => stringNormalizer(column)),
       bom: true,
     });
     this.data = normalizeEmptyValues(parsedData, unalterableColumns);
@@ -43,7 +50,7 @@ class CSVModule {
     logger.debug(`Get csvModule info by key '${key}'`);
     // return all rows if key and value aren't provided
     if (!key && !value) return this.data;
-    let result = this.data.filter((d) => d[key.toLowerCase()] === value);
+    let result = this.data.filter((d) => d[stringNormalizer(key)] === value);
     if (result.length === 0) {
       logger.warn(`CSV Record with provided key '${key}' and value was not found`);
       return result;
