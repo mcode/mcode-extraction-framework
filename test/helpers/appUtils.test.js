0c9b9aa3eab9d0204c60cd0e59b5dd3526001334
Julia Afeltra <jafeltra@mitre.org>
2021-06-15 15:16:01 -0400

    Add test for parsing patient id MRN with BOM

--- test/helpers/appUtils.test.js
+++ test/helpers/appUtils.test.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { parsePatientIds } = require('../../src/helpers/appUtils');
 
 const MOCK_VALID_ID_CSV = path.join(__dirname, './fixtures/valid-mrns.csv');
+const MOCK_VALID_ID_CSV_WITH_BOM = path.join(__dirname, './fixtures/valid-mrns-bom.csv');
 
 // Has no MRN column
 const MOCK_INVALID_ID_CSV = path.join(__dirname, './fixtures/invalid-mrns.csv');
@@ -13,6 +14,15 @@ describe('appUtils', () => {
       const ids = parsePatientIds(MOCK_VALID_ID_CSV);
 
       // Should get every MRN
+      expect(ids).toHaveLength(expectedIds.length);
+      expect(ids).toEqual(expectedIds);
+    });
+
+    test('valid path to CSV with BOM should parse content', () => {
+      const expectedIds = ['123', '456', '789'];
+      const ids = parsePatientIds(MOCK_VALID_ID_CSV_WITH_BOM);
+
+      // Should get every MRN and correctly parse with BOM
       expect(ids).toHaveLength(expectedIds.length);
       expect(ids).toEqual(expectedIds);
     });
