058d331eb7218b3990f75e8b09aa778e43ad30cb
Dylan Mendelowitz <dmendelowitz@mitre.org>
2021-06-28 11:48:18 -0400

    Adding debug level logs about config validation

--- src/helpers/configValidator.js
+++ src/helpers/configValidator.js
@@ -1,5 +1,6 @@
 const Ajv = require('ajv');
 const metaSchema = require('ajv/lib/refs/json-schema-draft-06.json');
+const logger = require('./logger');
 const configSchema = require('./schemas/config.schema.json');
 
 const ajv = new Ajv({ logger: false, allErrors: true });
@@ -7,9 +8,11 @@ ajv.addMetaSchema(metaSchema);
 const validator = ajv.addSchema(configSchema, 'config');
 
 function validateConfig(config) {
+  logger.debug('Validating config file');
   const valid = validator.validate('config', config);
   const errors = ajv.errorsText(validator.errors, { dataVar: 'config' });
   if (!valid) throw new Error(`Error(s) found in config file: ${errors}`);
+  logger.debug('Config file validated successfully');
 }
 
 module.exports = {
