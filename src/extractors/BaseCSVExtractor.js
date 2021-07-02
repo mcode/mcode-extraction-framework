599bd72c25fa2c49e87ec6215f07be055c01e4e7
Dylan Phelan <dtphelan1@gmail.com>
2021-07-02 14:17:09 -0400

    url takes precedence over filepath

--- src/extractors/BaseCSVExtractor.js
+++ src/extractors/BaseCSVExtractor.js
@@ -4,14 +4,14 @@ const { CSVFileModule, CSVURLModule } = require('../modules');
 class BaseCSVExtractor extends Extractor {
   constructor({ filePath, url, csvSchema, unalterableColumns }) {
     super();
-    this.unalterableColumns = unalterableColumns || [];
-    this.csvSchema = csvSchema;
-    if (filePath) {
-      this.filePath = filePath;
-      this.csvModule = new CSVFileModule(this.filePath, this.unalterableColumns);
-    } else if (url) {
+    if (url) {
+      this.unalterableColumns = unalterableColumns || [];
+      this.csvSchema = csvSchema;
       this.url = url;
       this.csvModule = new CSVURLModule(this.url, this.unalterableColumns);
+    } else if (filePath) {
+      this.filePath = filePath;
+      this.csvModule = new CSVFileModule(this.filePath, this.unalterableColumns);
     } else {
       throw new Error('Trying to instantiate a CSVExtractor without a filePath or url');
     }
