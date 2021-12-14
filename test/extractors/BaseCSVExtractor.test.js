7904ee656921e5cfd08a4b13e3f6124c046d3bb6
Dylan Phelan <dtphelan1@gmail.com>
2021-12-14 16:10:32 -0500

    removed placeholder tests

--- test/extractors/BaseCSVExtractor.test.js
+++ test/extractors/BaseCSVExtractor.test.js
@@ -31,34 +31,4 @@ describe('BaseCSVExtractor', () => {
         .toThrowError('Trying to instantiate a CSVExtractor without a valid filePath, url, or fileName+dataDirectory combination'));
     });
   });
-  // test('updateRequestHeaders calls its modules updateRequestHeaders function', () => {
-  //   moduleRequestHeadersSpy.mockClear();
-  //   baseFHIRExtractor.updateRequestHeaders(MOCK_REQUEST_HEADERS);
-  //   expect(moduleRequestHeadersSpy).toHaveBeenCalledWith(MOCK_REQUEST_HEADERS);
-  // });
-
-  // test('parametrizeArgsForFHIRModule parses data off of context if available', async () => {
-  //   baseFHIRModuleSearchSpy.mockClear();
-  //   const paramsBasedOnContext = await baseFHIRExtractor.parametrizeArgsForFHIRModule({ context: MOCK_CONTEXT });
-  //   expect(baseFHIRModuleSearchSpy).not.toHaveBeenCalled();
-  //   expect(paramsBasedOnContext).toHaveProperty('patient');
-  //   expect(paramsBasedOnContext.patient).toEqual(MOCK_CONTEXT.entry[0].resource.id);
-  // });
-
-  // test('parametrizeArgsForFHIRModule throws an error if context has no relevant ID', async () => {
-  //   baseFHIRModuleSearchSpy.mockClear();
-  //   await expect(baseFHIRExtractor.parametrizeArgsForFHIRModule({ context: {} })).rejects.toThrow();
-  //   expect(baseFHIRModuleSearchSpy).not.toHaveBeenCalled();
-  // });
-
-  // test('get should return a condition resource', async () => {
-  //   const data = await baseFHIRExtractor.get({ context: MOCK_CONTEXT });
-  //   expect(data.resourceType).toEqual('Bundle');
-  //   expect(data.entry).toBeDefined();
-  //   expect(data.entry.length).toBeGreaterThan(0);
-  //   expect(data.entry[0].resource.resourceType).toEqual('Condition');
-
-  //   // expect data to contain every element in the example
-  //   expect(data.entry).toEqual(expect.arrayContaining(exampleConditionBundle.entry));
-  // });
 });
